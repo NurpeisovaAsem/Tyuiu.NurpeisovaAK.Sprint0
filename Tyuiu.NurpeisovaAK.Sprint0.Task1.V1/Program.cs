787f695a12aefa53018c56e887ca825cd49b7264
Tyuiu.NurpeisovaAK@outlook.com <asemnurpeisova034@gmail.com>
2026-09-14 11:37:26 +0500

    Task1

--- Tyuiu.NurpeisovaAK.Sprint0.Task1.V1/Program.cs
+++ Tyuiu.NurpeisovaAK.Sprint0.Task1.V1/Program.cs
@@ -7,7 +7,7 @@
             Console.WriteLine("Hello, World!");
             string name = "Асем";
             int age = 17;
-            Console.WriteLine("Здравствуйте, меня зовут " + name + " мне " + age + " лет.");
+            Console.WriteLine("Здравствуйте, меня зовут " + name + " мне " + age + " лет!");
             Console.ReadKey();        
         }
     }
