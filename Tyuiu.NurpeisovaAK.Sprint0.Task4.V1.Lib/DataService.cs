1ca1cf40e4711310e7286a186de46b6677708be0
Tyuiu.NurpeisovaAK@outlook.com <asemnurpeisova034@gmail.com>
2026-09-14 18:27:32 +0500

    Task4

---
+++ Tyuiu.NurpeisovaAK.Sprint0.Task4.V1.Lib/DataService.cs
@@ -0,0 +1,22 @@
+﻿namespace Tyuiu.NurpeisovaAK.Sprint0.Task4.V1.Lib
+{
+    public class DataService
+    {
+        public static int Addition(int a,int b)
+        {
+            return a + b;
+        }
+        public static int Substraction(int a, int b)
+        {
+            return a - b;
+        }
+        public static int Multiplication(int a, int b)
+        {
+            return (a * b);
+        }
+        public static double Division(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
