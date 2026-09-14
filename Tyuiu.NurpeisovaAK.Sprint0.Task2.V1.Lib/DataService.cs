be39b2a58ad740257c7932c0e1ac5fb200894d9c
Tyuiu.NurpeisovaAK@outlook.com <asemnurpeisova034@gmail.com>
2026-09-14 14:36:00 +0500

    Task2

---
+++ Tyuiu.NurpeisovaAK.Sprint0.Task2.V1.Lib/DataService.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.NurpeisovaAK.Sprint0.Task2.V1.Lib
+{
+    public class DataService
+    {
+        public static string GetMessage(string name)
+        {
+            return $"Привет, {name}";
+        }
+
+    }
+}
---
+++ Tyuiu.NurpeisovaAK.Sprint0.Task2.V1.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.NurpeisovaAK.Sprint0.Task2.V1.Lib;
+namespace Tyuiu.NurpeisovaAK.Sprint0.Task2.V1.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            var name = "Асем";
+            var res = DataService.GetMessage(name);
+
+            Assert.AreEqual("Привет, Асем", res);
+
+        }
+    }
+}
